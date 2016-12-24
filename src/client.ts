b3fa76144ed0b86039eb69e4c6b9c6f155bbc4db
Sean Soowan Lee <sean@zoyi.co>
2016-12-24 16:47:48 +0900

    Add Client class

---
+++ src/client.ts
@@ -0,0 +1,8 @@
+export default class Client {
+    accessKey: string;
+    accessSecret: string;
+    constructor(accessKey: string, accessSecret: string) {
+        this.accessKey = accessKey
+        this.accessSecret = accessSecret
+    }
+}
---
+++ tests/client-test.ts
@@ -0,0 +1,12 @@
+import * as assert from 'assert';
+import { Client } from '../src'
+
+describe('Client', () => {
+    const accessKey = 'accessKey'
+    const accessSecret = 'accessSecret'
+    it('should construct with two fields', () => {
+        const client = new Client('accessKey', 'accessSecret')
+        assert.equal(accessKey, client.accessKey);
+        assert.equal(accessSecret, client.accessSecret);   
+    })    
+})
